ad5ee7f684e28f8c50bfef7ced454f77344ddc69
CizeLucas <cizelucas@gmail.com>
2025-01-20 22:27:50 -0300

    python process example code added

---
+++ Python/Python_Processos.py
@@ -0,0 +1,25 @@
+import multiprocessing
+import time
+import random
+
+def task(num):
+    print(f"Processo {num} iniciado")
+    sleepTime = random.randint(1, 4)
+    time.sleep(sleepTime)  # Simula tempo de processamento
+
+    if num == 2:  # Vamos provocar um erro no processo 2
+        raise Exception(f"Erro fatal no processo {num}")
+
+    print(f"Processo {num} concluido com sucesso (dormiu por {sleepTime} Segundos)")
+
+if __name__ == "__main__":
+    processes = []
+    for i in range(4):
+        process = multiprocessing.Process(target=task, args=(i,))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    print("Todos os processos finalizaram (com ou sem falhas)")
